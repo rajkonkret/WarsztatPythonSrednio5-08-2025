c190c8f09e2ac6476a06e69ed06f98e325238025
rajkonkret <rajkonkret660@gmail.com>
2025-08-05 14:18:10 +0200

    [1 7] update funkcje.py

--- dzien1/funkcje.py
+++ dzien1/funkcje.py
@@ -34,9 +34,29 @@ print(mnozenie(6, 9))  # 54
 zmienna = mnozenie(7, 43)
 print("Zmienna:", zmienna)  # Zmienna: 301
 
-
+# podpowiedz typów
 def mnozenie2(a: int, b: int) -> (int, int, int):
-    return a, b, a * b
+    return a, b, a * b  # zwraca krotke, tuple
+
 
 # _ * _ = _
-print()
+c = mnozenie2(8, 9)
+print(f"{c[0]} * {c[1]} = {c[2]}")  # 8 * 9 = 72
+
+wynik = mnozenie2(7, 9)
+print(wynik)  # (7, 9, 63)
+# rozpakowanie krotki
+a, b, wynik = mnozenie2(7, 9)
+print(f"{a} * {b} = {wynik}") # 7 * 9 = 63
+
+# ctrl alt l formatowanie wg PEP8
+print(mnozenie2("radek", 10))
+# ('radek', 10, 'radekradekradekradekradekradekradekradekradekradek')
+# pip install mypy zainstalowanie skryptu mypy
+#  cd .\dzien1\ przejscie do katalogu dzien1
+#  mypy .\funkcje.py uruchomienie mypy na naszym pliku
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\WarsztatPythonSrednio5-08-2025\dzien1> mypy .\funkcje.py
+# funkcje.py:38: error: Syntax error in type annotation  [syntax]
+# funkcje.py:38: note: Suggestion: Use Tuple[T1, ..., Tn] instead of (T1, ..., Tn)
+# funkcje.py:52: error: Argument 1 to "mnozenie2" has incompatible type "str"; expected "int"  [arg-type]
+# Found 2 errors in 1 file (checked 1 source file)
