6e4aafbfaf1657c7d5bdb26c3d79c2a842d31124
rajkonkret <rajkonkret660@gmail.com>
2025-08-06 15:17:15 +0200

    [2 19 update fun_zad_praca_wlasna

--- dzien2/funkcje_zad_praca_wlasna.py
+++ dzien2/funkcje_zad_praca_wlasna.py
@@ -1,8 +1,11 @@
-Zadanie 1
-
-Zbuduj plik wycieczka.py:
-Napisz funkcję kwota, w której policzysz kwotę do zapłaty za wycieczkę, która opiera się na rozliczeniu wartości takich jak cena za transport, nocleg i wyżywienie, wycieczki, ubezpieczenie i inne koszty, gdzie rabatowane są tylko dwie pierwsze wartości.
-
-Użyj funkcji do policzenia rabatu.
-
-Następnie zbuduj tablicę z różnymi wartościami rabatu i iteracyjnie wyznacz wartości do zapłaty dla stałych kosztów z zmiennego rabatu.
+# Zadanie 1
+#
+# Zbuduj plik wycieczka.py:
+# Napisz funkcję kwota, w której policzysz kwotę do zapłaty za wycieczkę,
+# która opiera się na rozliczeniu wartości takich jak cena za transport,
+# nocleg i wyżywienie, wycieczki, ubezpieczenie i inne koszty, gdzie rabatowane są tylko dwie pierwsze wartości.
+#
+# Użyj funkcji do policzenia rabatu.
+#
+# Następnie zbuduj listę z różnymi wartościami rabatu
+# i iteracyjnie wyznacz wartości do zapłaty dla stałych kosztów z zmiennego rabatu.
