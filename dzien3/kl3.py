5298c6dfc5c13f0c0735179f9c104596cb9f28d1
rajkonkret <rajkonkret660@gmail.com>
2025-08-07 12:58:36 +0200

    [3 12a] update kl3

--- dzien3/kl3.py
+++ dzien3/kl3.py
@@ -33,6 +33,15 @@ class Suplier(Contact):
 
 # klasa Friend, dziedziczy po Suplier
 # __repr__
+
+class Friend(Suplier):
+    """
+    Klasa dziedziczy po Suplier
+    """
+
+    def __repr__(self):  # repr nadpisuje str, gdy str nie jest zdefiniowany
+        return f"{self.name} {self.email}"
+
 c1 = Contact("Adam", "admin@wp.pl")
 print(c1)  # Adam admin@wp.pl
 c2 = Contact("Radek", "radek@wp.pl")
@@ -41,3 +50,12 @@ print(c1.all_contacts)
 # AttributeError: 'list' object has no attribute 'search'
 # po zdefiniowaniu listy ContactList
 print(c3.all_contacts.search("Radek"))  # [Radek radek@wp.pl]
+
+f1 = Friend("Kasia", "kasia@onet.pl")
+f2 = Friend("Kamil", "kamil@onet.pl")
+print(f1, f2)
+# Kasia kasia@onet.pl Kamil kamil@onet.pl
+
+print(Contact.all_contacts)
+# [Adam admin@wp.pl, Radek radek@wp.pl, Tomek tomek@wp.pl, Kasia kasia@onet.pl, Kamil kamil@onet.pl]
+print(Contact.all_contacts.search("Kasia")) # [Kasia kasia@onet.pl]
