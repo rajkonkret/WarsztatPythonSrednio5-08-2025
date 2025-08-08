96a14d77371b68bcd85ba3b85600615bf9d6069e
rajkonkret <rajkonkret660@gmail.com>
2025-08-08 15:11:45 +0200

    [4 27] add arkusz1.py

---
+++ dzien4/arkusz1.py
@@ -0,0 +1,31 @@
+import openpyxl
+import pandas as pd
+
+df = pd.DataFrame({"Osoba": ["Michał Jakub", "Ewa Noga", 'Krzysztof Zawierucha'],
+                   "Wynik": [15, 38, 21]})
+
+
+def sprawdz(punkty):
+    if punkty > 20:
+        return "Zdany"
+    else:
+        return "Oblany"
+
+
+print(df)
+#                   Osoba  Wynik
+# 0          Michał Jakub     15
+# 1              Ewa Noga     38
+# 2  Krzysztof Zawierucha     21
+
+df.Wynik = df.Wynik.apply(sprawdz)
+print(df)
+#                   Osoba   Wynik
+# 0          Michał Jakub  Oblany
+# 1              Ewa Noga   Zdany
+# 2  Krzysztof Zawierucha   Zdany
+print(df.columns)  # Index(['Osoba', 'Wynik'], dtype='object')
+
+df.to_csv("wynik.csv")
+df.to_excel("wynik.xlsx")
+df.to_excel("wynik_bez_indexu.xlsx", index=False)
