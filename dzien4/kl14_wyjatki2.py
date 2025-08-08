d3faebc50ec72b38b9858a36fda25a0fd8d2b40a
rajkonkret <rajkonkret660@gmail.com>
2025-08-08 11:46:41 +0200

    [4 12] add kl_14_wyjatki.py

---
+++ dzien4/kl14_wyjatki2.py
@@ -0,0 +1,25 @@
+class MyError(Exception):
+    def __init__(self, message, err_code):
+        super().__init__(message)
+        self.err_code = err_code
+
+
+class MyValueError(MyError):
+    def __init__(self, message):
+        super().__init__(message, err_code=100)
+
+
+class MyTypeError(MyError):
+    def __init__(self, message):
+        super().__init__(message, err_code=200)
+
+
+def my_function(x: int, y: int) -> float:
+    if not isinstance(x, int):
+        raise MyTypeError("X must be integer")
+    if not isinstance(y, int):
+        raise MyTypeError("Y must be integer")
+    if y == 0:
+        raise MyValueError("y cannot be zero")
+
+    return x / y
