bbe82f119d8613989a17101d351689466a20a54f
rajkonkret <rajkonkret660@gmail.com>
2025-08-08 14:27:53 +0200

    [4 24] update api_nbp.json

--- dzien4/api_nbp.py
+++ dzien4/api_nbp.py
@@ -1,8 +1,24 @@
+import time
+
 import requests
 
 url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
 
-response = requests.get(url)
-print(response)
-data = response.json()
-print(data)
+
+# response = requests.get(url)
+# print(response)
+# data = response.json()
+# print(data)
+
+def multiple_requests():
+    start_time = time.perf_counter()
+
+    for _ in range(100):
+        response = requests.get(url)
+        print(response.json())
+
+    elapsed = time.perf_counter() - start_time
+    print(f"Requests time: {elapsed:.4f} ")
+
+# Requests time: 0.0445  dla jedego
+multiple_requests() # Requests time: 4.5089
